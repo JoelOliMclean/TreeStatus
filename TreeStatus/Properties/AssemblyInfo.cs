547c50d222784de30f1ff9e5ffc0ad085bd1cca1
Joel Mclean <joel@oliapps.co.uk>
2021-03-07 01:53:05 +0000

    Initial commit

---
+++ TreeStatus/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TreeStatus")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TreeStatus")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f735d9b-dc72-4927-afbe-24d680fbb799")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TreeStatus/TreeStatus.cs
@@ -0,0 +1,62 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace TreeStatus
+{
+    [BepInPlugin("uk.co.oliapps.valheim.treestatus", "Tree Status", "0.0.1")]
+    public class TreeStatus : BaseUnityPlugin
+    {
+        public enum DisplayType { HealthBar, Percentage, Disabled }
+
+        private static ConfigEntry<DisplayType> displayType;
+        public void Awake()
+        {
+            displayType = Config.Bind("General", "Display Type", DisplayType.HealthBar, "Desired display type for tree health status");
+            Config.Save();
+            Harmony.CreateAndPatchAll(typeof(TreeStatus), null);
+        }
+
+        [HarmonyPatch(typeof(TreeBase), "RPC_Damage")]
+        [HarmonyPostfix]
+        public static void TreeBase_RPC_Damage(ref TreeBase __instance, long sender, HitData hit)
+        {
+            if (!displayType.Value.Equals(DisplayType.Disabled))
+            {
+                var m_nview = (ZNetView)AccessTools.Field(typeof(TreeBase), "m_nview").GetValue(__instance);
+                float remainingHealth = m_nview.GetZDO().GetFloat("health", __instance.m_health);
+                float remainingPercentage = remainingHealth / __instance.m_health * 100;
+                Chat.instance.SetNpcText(__instance.gameObject, Vector3.up, 0, 5.0f, "", GetPercentageString(remainingPercentage), false);
+            }
+        }
+
+        private static string GetPercentageString(float percentage)
+        {
+            switch (displayType.Value)
+            {
+                case DisplayType.HealthBar:
+                    float percentageBase2 = percentage / 10;
+                    string progress = "";
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        if (i <= percentageBase2)
+                        {
+                            progress += "<color=#00CC00>█</color>";
+                        }
+                        else
+                        {
+                            progress += "<color=#CC0000>█</color>";
+                        }
+                    }
+                    return progress;
+                case DisplayType.Percentage:
+                default:
+                    return $"<color=orange>{Convert.ToInt32(percentage)}% left</color>";
+
+            }
+            
+        }
+    }
+}
