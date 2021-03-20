4a932d88e9571a5c8cd7495aa9619848256f068b
Joel Mclean <joelolimclean@gmail.com>
2021-03-20 18:02:41 +0000

    1.0.0
    - Added visual onto fallen logs!
    - Added more configurability
    - Health now properly displays more accurately (was 1 hit behind)
    - Implemented better fix for issue "kinda" fixed in v0.0.3

--- TreeStatus/TreeStatus.cs
+++ TreeStatus/TreeStatus.cs
@@ -7,30 +7,59 @@ using UnityEngine;
 
 namespace TreeStatus
 {
-    [BepInPlugin("uk.co.oliapps.valheim.treestatus", "Tree Status", "0.0.3")]
+    [BepInPlugin("uk.co.oliapps.valheim.treestatus", "Tree Status", "1.0.0")]
     public class TreeStatus : BaseUnityPlugin
     {
         public enum DisplayType { HealthBar, Percentage, Disabled }
 
         private static ConfigEntry<DisplayType> displayType;
+        private static ConfigEntry<bool> modEnabled;
+        private static ConfigEntry<bool> showOnLog;
+        private static ConfigEntry<bool> showOnTree;
+
         public void Awake()
         {
             displayType = Config.Bind("General", "Display Type", DisplayType.HealthBar, "Desired display type for tree health status");
+            modEnabled = Config.Bind("General", "Mod enabled", true, "Sets whether or not the mod is enabled");
+            showOnLog = Config.Bind("General", "Show on logs", true, "Sets whether not to show damage visual on felled logs");
+            showOnTree = Config.Bind("General", "Show on trees", true, "Sets whether not to show damage visual on trees");
             Config.Save();
-            Harmony.CreateAndPatchAll(typeof(TreeStatus), null);
+            if (modEnabled.Value)
+            {
+                Harmony.CreateAndPatchAll(typeof(TreeStatus), null);
+            }
         }
 
 
         [HarmonyPatch(typeof(TreeBase), "RPC_Damage")]
-        [HarmonyPrefix]
-        public static bool TreeBase_Prefix_RPC_Damage(ref TreeBase __instance, long sender, HitData hit)
+        [HarmonyPostfix]
+        public static void TreeBase_Postfix_RPC_Damage(ref TreeBase __instance, long sender, HitData hit)
         {
-            if (!displayType.Value.Equals(DisplayType.Disabled))
+            if (__instance && showOnTree.Value)
             {
-                if (__instance)
+                if (!displayType.Value.Equals(DisplayType.Disabled))
                 {
-                    var m_nview = (ZNetView)AccessTools.Field(typeof(TreeBase), "m_nview").GetValue(__instance);
-                    if (m_nview)
+                    var m_nview = AccessTools.Field(typeof(TreeBase), "m_nview").GetValue(__instance) as ZNetView;
+                    if (m_nview.IsValid())
+                    { 
+                        float remainingHealth = m_nview.GetZDO().GetFloat("health", __instance.m_health);
+                        float remainingPercentage = remainingHealth / __instance.m_health * 100;
+                        Chat.instance.SetNpcText(__instance.gameObject, Vector3.up, 0, 5.0f, "", GetPercentageString(remainingPercentage), false);
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(TreeLog), "RPC_Damage")]
+        [HarmonyPostfix]
+        public static void TreeLog_Postfix_RPC_Damage(ref TreeLog __instance, long sender, HitData hit)
+        {
+            if (__instance && showOnLog.Value)
+            {
+                if (!displayType.Value.Equals(DisplayType.Disabled))
+                {
+                    var m_nview = AccessTools.Field(typeof(TreeLog), "m_nview").GetValue(__instance) as ZNetView;
+                    if (m_nview.IsValid())
                     {
                         float remainingHealth = m_nview.GetZDO().GetFloat("health", __instance.m_health);
                         float remainingPercentage = remainingHealth / __instance.m_health * 100;
@@ -38,7 +67,6 @@ namespace TreeStatus
                     }
                 }
             }
-            return true;
         }
 
         private static string GetPercentageString(float percentage)
